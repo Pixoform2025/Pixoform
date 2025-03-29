275afe8176a11609a8793cb072196cc67aa61c73
Pixoform2025 <pixoform@gmail.com>
2025-03-29 12:05:47 +0530

    changed to why choose us grid layout

--- src/components/WhyChooseSection.tsx
+++ src/components/WhyChooseSection.tsx
@@ -156,7 +156,7 @@ const WhyChooseSection: React.FC = () => {
           ))}
         </div>
 
-        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {features.map((feature, index) => (
             <div 
               key={feature.title}
